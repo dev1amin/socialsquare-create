1803f84babc9281686afab4ad9f742255175f813
Mateus Henrique Soares Leão <324115118@ulife.com.br>
2025-11-23 21:28:25 +0000

    Ajustes gerais

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import OnboardFunnel from './components/OnboardFunnel';
 import VoiceToneFunnel from './components/VoiceToneFunnel';
+import AccountCreationForm from './components/AccountCreationForm';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/create-account" element={<AccountCreationForm />} />
         <Route path="/onboard" element={<OnboardFunnel />} />
         <Route path="/voice-tone" element={<VoiceToneFunnel />} />
-        <Route path="/" element={<Navigate to="/onboard" replace />} />
+        <Route path="/" element={<Navigate to="/create-account" replace />} />
       </Routes>
     </Router>
   );
